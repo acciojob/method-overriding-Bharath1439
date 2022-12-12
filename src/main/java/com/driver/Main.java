ef220cffd7cc97ea676167eaafa15dd7360e7ad5
bharath <vaddibharath2000@gmail.com>
2022-12-12 08:43:59 +0530

    fourth time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
 
     public static class A {
-        public String math(){
+        public String meth(){
             return "Invoking method from class A";
         }
     }
